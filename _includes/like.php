27c64a70be48889c958e43145e4068ebbd6464d8
Wallace Erick <wallace_erick@hotmail.com>
2015-03-02 16:51:08 -0300

    ajuste like

--- _includes/like.php
+++ _includes/like.php
@@ -1,5 +1,5 @@
 <?php $url = basename($_SERVER['PHP_SELF']); ?>
-<section id="like" class="dark">
+<section id="like" class="dark animated-content js-animated-content">
 			<h4 class="title">Gostou do meu trabalho?</h4>
 			<h5 class="subtitle">Podemos ser <span>excelentes parceiros</span></h5>
 			<a href="<?php echo $base_url ?>contact">
