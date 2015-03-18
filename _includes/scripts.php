6728c21c52338c55d7f94af47eec4362b63af872
Wallace Erick <wallace_erick@hotmail.com>
2015-03-18 17:13:44 -0300

    scripts

--- _includes/scripts.php
+++ _includes/scripts.php
@@ -1,9 +1,9 @@
 <!-- <audio class="js-audio-player" src="<?php echo $base_url ?>content/audio/smoke.mp3" type="audio/mp3"></audio> -->
 
 		<script src="<?php echo $base_url ?>content/js/application.js"></script>
-		<?php if($url == "contact.php"){ ?>
+<?php if($url == "contact.php"){ ?>
 			<script src="<?php echo $base_url ?>content/js/_form.js"></script>
-		<?php } ?>
+<?php } ?>
 		<script>
 			(function(d) {
 				var config = {
@@ -12,6 +12,16 @@
 				},
 				h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 			})(document);
+		</script>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+				_gaq.push(['_setAccount', 'UA-19247596-1']);
+				_gaq.push(['_trackPageview']);
+				(function() {
+					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
 		</script>
 		
 		<?php if($base_url == "http://localhost/~wallace/portfolio/"){ ?>
