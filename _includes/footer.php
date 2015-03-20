16f5d14cd28577d210c1282f9df47cf266531202
Wallace Erick <wallace_erick@hotmail.com>
2015-03-20 10:49:44 -0300

    update ga tracking and htaccess woff

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,44 +1,44 @@
 <footer id="footer" class="animated-content js-animated-content">
 			<ul class="social">
 				<li>
-					<a href="https://www.facebook.com/wallace.erick">
+					<a href="https://www.facebook.com/wallace.erick" onclick="ga('send', 'event', {eventCategory: 'Social', eventAction: 'Facebook'});">
 						<i class="icon icon-facebook"></i>
 						<span>Facebook</span>
 					</a>
 				</li>
 				<li>
-					<a href="https://twitter.com/wallaceerick">
+					<a href="https://twitter.com/wallaceerick" onclick="ga('send', 'event', {eventCategory: 'Social', eventAction: 'Twitter'});">
 						<i class="icon icon-twitter"></i>
 						<span>Twitter</span>
 					</a>
 				</li>
 				<li>
-					<a href="https://www.linkedin.com/in/wallaceerick">
+					<a href="https://www.linkedin.com/in/wallaceerick" onclick="ga('send', 'event', {eventCategory: 'Social', eventAction: 'Linkedin'});">
 						<i class="icon icon-linkedin"></i>
 						<span>Linkedin</span>
 					</a>
 				</li>
 				<li>
-					<a href="http://codepen.io/wallaceerick/">
+					<a href="http://codepen.io/wallaceerick/" onclick="ga('send', 'event', {eventCategory: 'Social', eventAction: 'Codepen'});">
 						<i class="icon icon-codepen"></i>
 						<span>Codepen</span>
 					</a>
 				</li>
 				<li>
-					<a href="https://github.com/wallaceerick/">
+					<a href="https://github.com/wallaceerick/" onclick="ga('send', 'event', {eventCategory: 'Social', eventAction: 'Github'});">
 						<i class="icon icon-github"></i>
 						<span>Github</span>
 					</a>
 				</li>
 				<li>
-					<a href="bitbucket.org/wallaceerick">
+					<a href="bitbucket.org/wallaceerick" onclick="ga('send', 'event', {eventCategory: 'Social', eventAction: 'BitBucket'});">
 						<i class="icon icon-bitbucket"></i>
 						<span>BitBucket</span>
 					</a>
 				</li>
 			</ul>
 			
-			<a href="mailto:wallaceerick.dev@gmail.com?subject=Contato pelo Site" class="quick-mail">wallaceerick.dev@gmail.com</a>
+			<a href="mailto:wallaceerick.dev@gmail.com?subject=Contato pelo Site" class="quick-mail" onclick="ga('send', 'event', {eventCategory: 'Button', eventAction: 'Send Mail'});">wallaceerick.dev@gmail.com</a>
 			<p>Copyright &copy; 2008/2015. <span>Alguns Direitos Reservados.</span></p>
 			<a href="#" class="to-top js-to-top" onclick="ga('send', 'event', {eventCategory: 'Button', eventAction: 'Back to Top'});">
 				<i class="icon icon-arrow-up"></i>
